37321103b46c74a678ef304a786b1c9b2c9c7343
Maximilien Fathi <maximilian.fathi@gmail.com>
2022-07-20 22:27:43 -0400

    Create search-bar.js file

---
+++ src/components/search-bar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function SearchBar() {
+  return (
+    <div className="search-container">
+      <form className="search-form">
+        <input className="search-bar" placeholder="Search" />;
+      </form>
+    </div>
+  );
+}
+
+export default SearchBar;
