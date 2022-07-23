7479e2fcb8c830c6922169a57ae1a36efebf99e8
Maximilien Fathi <maximilian.fathi@gmail.com>
2022-07-23 17:27:45 -0400

    Create statistic.js file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from "./components/header";
+import Hero from "./components/hero";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Hero></Hero>
     </div>
   );
 }
---
+++ src/components/hero.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Hero() {
+  return (
+    <div>
+      <h1>An easy way to track and trade cryptocurrencies</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aperiam
+        assumenda atque culpa cum delectus earum enim facere fugit impedit iure
+        iusto obcaecati, odio optio possimus praesentium rerum tempore velit!
+      </p>
+    </div>
+  );
+}
+
+export default Hero;
