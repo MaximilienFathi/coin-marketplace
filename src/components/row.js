24ff8a0ca50d91fb370da98ce3226ce180d36cce
Maximilien Fathi <maximilian.fathi@gmail.com>
2022-07-26 22:43:51 -0400

    Update Table and Row components

--- src/components/row.js
+++ src/components/row.js
@@ -1,7 +1,40 @@
 import React from "react";
 
-function Row(props) {
-  return <div></div>;
+function Row({
+  image,
+  name,
+  symbol,
+  current_price,
+  price_change_percentage_24h,
+  total_volume,
+  market_cap,
+}) {
+  const color = current_price > 0 ? "green" : "red";
+
+  const transformData = function (data, fractionDigits = 0) {
+    const numberFormatter = Intl.NumberFormat("en-US");
+    return numberFormatter.format(data.toFixed(fractionDigits));
+  };
+
+  return (
+    <div className="table-row">
+      <div>
+        <img
+          className="coin-logo"
+          src={image}
+          alt="logo of the cryptocurrency"
+        ></img>
+        <p className="coin-name">{name}</p>
+        <p className="coin-symbol">{symbol}</p>
+      </div>
+      <p className="coin-price">${transformData(current_price, 2)}</p>
+      <p className={`coin-price-change ${color}`}>
+        {transformData(price_change_percentage_24h, 2)}%
+      </p>
+      <p className="coin-volume">${transformData(total_volume)}</p>
+      <p className="coin-market-cap">${transformData(market_cap)}</p>
+    </div>
+  );
 }
 
 export default Row;
--- src/components/table.js
+++ src/components/table.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useInsertionEffect, useState } from "react";
 import axios from "axios";
+import Row from "./row";
 
 function Table(props) {
   const [coins, setCoins] = useState([]);
@@ -7,18 +8,35 @@ function Table(props) {
   useEffect(() => {
     axios
       .get(
-        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=cad&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
       )
       .then((res) => {
         setCoins(res.data);
-        console.log(res);
+        console.log(res.data);
       })
       .catch((err) => {
         console.error(err);
       });
   }, []);
 
-  return <div></div>; // {coins.map((coin) => {return(<Row>)})}
+  return (
+    <div className="table">
+      {coins.map((coin) => {
+        return (
+          <Row
+            key={coin.id}
+            image={coin.image}
+            name={coin.name}
+            symbol={coin.symbol}
+            current_price={coin.current_price}
+            price_change_percentage_24h={coin.price_change_percentage_24h}
+            total_volume={coin.total_volume}
+            market_cap={coin.market_cap}
+          ></Row>
+        );
+      })}
+    </div>
+  );
 }
 
 export default Table;
