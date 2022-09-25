bfff5c9a446f0f23f2954f864973b5c3e1c3356a
Maximilien Fathi <maximilian.fathi@gmail.com>
2022-09-24 21:39:58 -0400

    Add and style feature-card component

---
+++ src/components/home/feature-card/feature-card.js
@@ -0,0 +1,31 @@
+import React from "react";
+import ArrowOutwardIcon from "@mui/icons-material/ArrowOutward";
+import "./feature-card.css";
+
+//========================================================
+// CUSTOM STYLES
+const IconStyles = {
+  width: "1.8rem",
+  height: "auto",
+};
+//========================================================
+
+function FeatureCard({ picture, title, text }) {
+  return (
+    <div className="feature-container">
+      <img
+        className="feature-picture"
+        src={picture}
+        alt={`picture of a feature`}
+      ></img>
+      <p className="feature-title">{title}</p>
+      <p className="feature-text">{text}</p>
+      <button className="feature-button">
+        <p>Read More</p>
+        <ArrowOutwardIcon sx={IconStyles}></ArrowOutwardIcon>
+      </button>
+    </div>
+  );
+}
+
+export default FeatureCard;
--- src/components/home/features/features.js
+++ src/components/home/features/features.js
@@ -1,4 +1,5 @@
 import React from "react";
+import FeatureCard from "../feature-card/feature-card";
 import "./features.css";
 
 function Features() {
@@ -7,9 +8,27 @@ function Features() {
       <h3 className="features-subheading">Features</h3>
       <h2 className="features-heading">What is it all about?</h2>
       <div className="features-inner-container">
-        <div className="feature-box"></div>
-        <div className="feature-box"></div>
-        <div className="feature-box"></div>
+        <FeatureCard
+          picture={require("../Time machine-amico.png")}
+          title="Crypto Tracking"
+          text="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore illo itaque laboriosam minus nihil porro repellat."
+        ></FeatureCard>
+        <FeatureCard
+          picture={require("../Dark analytics-bro.png")}
+          title={"Crypto Analysis"}
+          text={
+            "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi" +
+            " autem blanditiis cum dolorum facere impedit consectetur."
+          }
+        ></FeatureCard>
+        <FeatureCard
+          picture={require("../Bitcoin-amico.png")}
+          title={"Crypto Trading"}
+          text={
+            "Lorem ipsum dolor sit amet, consectetur adipisicing elit." +
+            " Corporis distinctio doloremque, eveniet facilis inventore."
+          }
+        ></FeatureCard>
       </div>
     </div>
   );
