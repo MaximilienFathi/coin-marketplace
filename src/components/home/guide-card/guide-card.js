9553d87d071a56d19bc579b0a18006a7d7b8bda7
Maximilien Fathi <maximilian.fathi@gmail.com>
2022-09-26 23:34:57 -0400

    Update quick-guide component

--- src/components/home/guide-card/guide-card.js
+++ src/components/home/guide-card/guide-card.js
@@ -1,8 +1,23 @@
 import React from "react";
 import "./guide-card.css";
 
-function GuideCard(props) {
-  return <div></div>;
+function GuideCard({ picture, number, heading, description }) {
+  return (
+    <div className="quick-guide-step-box">
+      <div className="quick-guide-step-text">
+        <p className="quick-guide-step-number">{number}</p>
+        <h3 className="quick-guide-step-heading">{heading}</h3>
+        <p className="quick-guide-step-description">{description}</p>
+      </div>
+      <div className="quick-guide-step-img">
+        <img
+          className="guide-step-picture"
+          src={picture}
+          alt={`picture for ${heading} step`}
+        ></img>
+      </div>
+    </div>
+  );
 }
 
 export default GuideCard;
--- src/components/home/quick-guide/quick-guide.js
+++ src/components/home/quick-guide/quick-guide.js
@@ -9,34 +9,86 @@ function QuickGuide() {
       <h2 className="quick-guide-heading">Your Crypto Journey Starts Now</h2>
       <div className="quick-guide-inner-container">
         <GuideCard
-          picture={require("../Time machine-amico.png")}
-          title="Crypto Tracking"
-          text="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore illo itaque laboriosam minus nihil porro repellat."
+          picture={require("../Plain credit card-amico.png")}
+          number="01"
+          heading="Create a wallet"
+          description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore illo itaque laboriosam minus nihil porro repellat."
         ></GuideCard>
         <GuideCard
-          picture={require("../Dark analytics-bro.png")}
-          title={"Crypto Analysis"}
-          text={
+          picture={require("../Bitcoin-cuate.png")}
+          number="02"
+          heading="Send cryptos to your wallet"
+          description={
             "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi" +
             " autem blanditiis cum dolorum facere impedit consectetur."
           }
         ></GuideCard>
         <GuideCard
-          picture={require("../Bitcoin-amico.png")}
-          title={"Crypto Trading"}
-          text={
+          picture={require("../Bitcoin P2P-amico.png")}
+          number="03"
+          heading={"Connect wallet to coin marketplace"}
+          description={
             "Lorem ipsum dolor sit amet, consectetur adipisicing elit." +
             " Corporis distinctio doloremque, eveniet facilis inventore."
           }
         ></GuideCard>
         <GuideCard
-          picture={require("../Bitcoin-amico.png")}
-          title={"Crypto Trading"}
-          text={
+          picture={require("../Bitcoin P2P-pana.png")}
+          number="04"
+          heading={"Swap your cryptos for other cryptos"}
+          description={
             "Lorem ipsum dolor sit amet, consectetur adipisicing elit." +
             " Corporis distinctio doloremque, eveniet facilis inventore."
           }
         ></GuideCard>
+        {/*/!* STEP 2 *!/*/}
+        {/*<div className="quick-guide-img-box">*/}
+        {/*  <img*/}
+        {/*    className="guide-step-picture"*/}
+        {/*    src={require("../Plain credit card-amico.png")}*/}
+        {/*    alt={`picture for creating a wallet`}*/}
+        {/*  ></img>*/}
+        {/*</div>*/}
+        {/*<div className="quick-guide-text-box">*/}
+        {/*  <p>02</p>*/}
+        {/*  <h3>Create a wallet</h3>*/}
+        {/*  <p>*/}
+        {/*    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam*/}
+        {/*    autem delectus deserunt dicta distinctio.*/}
+        {/*  </p>*/}
+        {/*</div>*/}
+        {/*/!* STEP 3 *!/*/}
+        {/*<div className="quick-guide-text-box">*/}
+        {/*  <p>03</p>*/}
+        {/*  <h3>Create a wallet</h3>*/}
+        {/*  <p>*/}
+        {/*    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam*/}
+        {/*    autem delectus deserunt dicta distinctio.*/}
+        {/*  </p>*/}
+        {/*</div>*/}
+        {/*<div className="quick-guide-img-box">*/}
+        {/*  <img*/}
+        {/*    className="guide-step-picture"*/}
+        {/*    src={require("../Plain credit card-amico.png")}*/}
+        {/*    alt={`picture for creating a wallet`}*/}
+        {/*  ></img>*/}
+        {/*</div>*/}
+        {/*/!* STEP 4 *!/*/}
+        {/*<div className="quick-guide-img-box">*/}
+        {/*  <img*/}
+        {/*    className="guide-step-picture"*/}
+        {/*    src={require("../Plain credit card-amico.png")}*/}
+        {/*    alt={`picture for creating a wallet`}*/}
+        {/*  ></img>*/}
+        {/*</div>*/}
+        {/*<div className="quick-guide-text-box">*/}
+        {/*  <p>04</p>*/}
+        {/*  <h3>Create a wallet</h3>*/}
+        {/*  <p>*/}
+        {/*    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam*/}
+        {/*    autem delectus deserunt dicta distinctio.*/}
+        {/*  </p>*/}
+        {/*</div>*/}
       </div>
     </div>
   );
